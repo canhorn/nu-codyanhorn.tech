687c93769774160ca3562ed767fcb553b5f7bc28
Cody Merritt Anhorn <cody.anhorn@hotmail.com>
2021-08-05 10:45:53 -0500

    Added Contact Page. Added Copy feature to code blocks.

--- Data/PageMeta.cs
+++ Data/PageMeta.cs
@@ -4,6 +4,7 @@
     {
         public OpenGraph OpenGraph { get; set; } = new OpenGraph();
         public UrlSlug Home { get; set; } = new UrlSlug();
+        public UrlSlug Contact { get; set; } = new UrlSlug();
         public UrlSlug BlogIndex { get; set; } = new UrlSlug();
         public UrlSlug BlogIndexPage { get; set; } = new UrlSlug();
         public UrlSlug Post { get; set; } = new UrlSlug();
